8a2b1a24af7ff6d471e6d1bf4f2767a5feebc07d
theo <me@t3.gg>
2022-06-02 23:18:35 -0700

    add maxage for testing

--- app/routes/growthbook-cache-mirror.ts
+++ app/routes/growthbook-cache-mirror.ts
@@ -8,7 +8,7 @@ export const loader = async () => {
     status: 200,
     headers: {
       "Content-Type": "application/json",
-      "Cache-Control": "stale-while-revalidate=60",
+      "Cache-Control": "stale-while-revalidate=60, s-maxage=60",
     },
   });
 };

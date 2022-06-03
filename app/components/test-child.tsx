90a6097daa0dc3448bf1a043b1876dd8c5548dcb
theo <me@t3.gg>
2022-06-02 22:21:20 -0700

    rename test feature

--- app/components/test-child.tsx
+++ app/components/test-child.tsx
@@ -1,7 +1,7 @@
 import { useFeature } from "@growthbook/growthbook-react";
 
 export const DemoComponent = () => {
-  const { on: featureOn } = useFeature("nps-enabled");
+  const { on: featureOn } = useFeature("test-feature");
 
   if (featureOn) return <div>Feature On</div>;
 
